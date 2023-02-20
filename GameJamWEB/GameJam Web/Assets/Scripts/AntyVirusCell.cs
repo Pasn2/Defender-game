0af9868c161bd38a6717b615585354e874f4d4f1
Pasn2 <101821135+Pasn2@users.noreply.github.com>
2023-02-20 20:59:09 +0100

    fix remove infection, change card, add info

--- GameJamWEB/GameJam Web/Assets/Scripts/AntyVirusCell.cs
+++ GameJamWEB/GameJam Web/Assets/Scripts/AntyVirusCell.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AntyVirusCell : MonoBehaviour,IDamagable
 {
+    [SerializeField] float infectionRate;
     public void TakeDamage(float _damage)
     {
         Dead();
@@ -12,15 +13,15 @@ public class AntyVirusCell : MonoBehaviour,IDamagable
     public void Dead(){
         Destroy(gameObject);
     }    
-    private void OnCollisionEnter2D(Collision2D other) {
-        print(other.collider.tag);
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         switch(other.collider.tag){
             case "Opponent":
                 other.collider.GetComponent<IDead>().Dead();
                 Dead();
             break;
             case "Computer":
-                other.collider.GetComponent<Computer>().RemoveInfection(30f);
+                other.collider.GetComponent<Computer>().RemoveInfection(infectionRate);
                 Dead();
             break;
         }
